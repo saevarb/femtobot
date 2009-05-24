6ac20c85ab1ead4e775a7b368660eb11934be214
wouter.h.f <wouter.h.f@d742501c-4806-11de-a4f5-9d9bc3506571>
2009-05-24 21:59:51 +0000

    fixed type and arrangement in structs.h

--- config_parsing.c
+++ config_parsing.c
@@ -1,8 +1,56 @@
-#include <stdio.h>
-#include <string.h>
+#include "config_parsing.h"
 
 int get_settings(char *filename)
 {
 	FILE *in;
-	but
+	unsigned int count = 0, buffer_size = BUFFER_INCREMENTS;
+	char *buffer, one;
+		
+	in = fopen(filename, "rt");
+	if(in == NULL)
+	{
+		fprintf(stderr, "Error: failed to open config file `%s`.", filename);
+		return -1;
+	}
+	
+	buffer = calloc(BUFFER_INCREMENTS, sizeof(char));
+	if(buffer == NULL)
+	{
+		fprintf(stderr, "Error: Memory allocation failed.");
+		exit(1);
+	}
+	
+	while((one = getc(in)) != EOF)
+	{
+		if(one == EOL)
+		{
+			buffer[count + 1] = '\0';
+			free(buffer);
+			count = 0;
+			buffer_size = BUFFER_INCREMENTS;
+			buffer = calloc(BUFFER_INCREMENTS, sizeof(char));
+			if(buffer == NULL)
+			{
+				fprintf(stderr, "Error: Memory allocation failed.");
+				exit(1);
+			}
+		}
+		
+		if(count >= (buffer_size - 2))
+		{
+			buffer_size += BUFFER_INCREMENTS;
+			buffer = realloc(buffer, buffer_size);
+			if(buffer == NULL)
+			{
+				fprintf(stderr, "Error: Memory allocation failed.");
+				exit(1);
+			}
+		}
+		buffer[count] = one;
+		printf("%02x = %02x (%u)\n", one, buffer[count], count);
+		count++;
+		buffer[count] = 0;
+		printf("buffer: %s\n", buffer);
+	}
+	return 0;
 }
--- config_parsing.h
+++ config_parsing.h
@@ -1,6 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "structs.h"
 
-#ifdef __CONFIG_PARSING_H_
+#ifndef __CONFIG_PARSING_H_
 #define __CONFIG_PARSING_H_
-	int get_settings(char *filename);
+
+#define BUFFER_INCREMENTS 64
+#define EOL '\n'
+
+int get_settings(char *filename);
+
+
 #endif
