a03e2baae35cd8793835160613820d199a3a6b19
wouter.h.f <wouter.h.f@d742501c-4806-11de-a4f5-9d9bc3506571>
2009-05-24 21:00:30 +0000

    added structs for the config parser

--- trunk/config_parsing.c
+++ trunk/config_parsing.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct _serverinfo
-{
-	char **nick;
-	char *host;
-	unsigned short int port;
-	channel *channels;
-}botinfo;
-
-typedef struct _channel
-{
-	char *channelname;
-	char *password;
-}channel;
-
 int get_settings(char *filename)
 {
 	FILE *in;
