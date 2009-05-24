2627fcdd1a8e9f6f20e4bf92e3beb1e0db84cb17
wouter.h.f <wouter.h.f@d742501c-4806-11de-a4f5-9d9bc3506571>
2009-05-24 20:32:10 +0000

    basic structs for config

--- config_parsing.c
+++ config_parsing.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <string.h>
 
+typedef struct _serverinfo
+{
+	char **nick;
+	char *host;
+	unsigned short int port;
+	channel *channels;
+}botinfo;
+
+typedef struct _channel
+{
+	char *channelname;
+	char *password;
+}channel;
+
+int get_settings(char *filename)
+{
+	FILE *in;
+	but
+}
