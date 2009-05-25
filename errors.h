667da09a997604b22d77171ea1fa810364953b57
icedane <icedane@d742501c-4806-11de-a4f5-9d9bc3506571>
2009-05-25 02:04:31 +0000

    Added more error constants to error.h Added error_handling.* for future implementation of error handling functions. Wrote a main.c to test config_parsing.c functionality Modified the bot.conf for proper testing of config_parsing.c functions. Added bot_config structure to structures.h Added prototypes for new functions in config_parsing.c to prototypes.h.

--- errors.h
+++ errors.h
@@ -1,2 +1,14 @@
 
-#define ERR_CHECK_ERRNO     0x0001
+/* Define error constants here */
+
+/* Means the erring function uses errno 
+ * report error. The error can thus be 
+ * retrieved with strerror */
+#define ERR_CHECK_ERRNO    	 0x0001
+/* Means a setting in the config file
+ * was defined twice. */
+#define ERR_ALRDY_DEFINED  	 0x0002
+/* Invalid config line */
+#define ERR_INVALID_LINE 	 0x0003
+/* Line is a comment */
+#define ERR_COMMENT_LINE	 0x0004
--- main.c
+++ main.c
@@ -2,8 +2,37 @@
 
 int main(void)
 {
+	bot_info b_info;
+	bot_config *setting_ptr;
+	int i;
 	
-	int ret = get_settings("bot.conf");
+	/* Mandatory intialization. Failure to do this
+	 * may result in havoc later on. Especially
+	 * if you are using the _count variables without 
+	 * properly intializing them to zero */
+	b_info.server_count = 0;
+	b_info.nick_count = 0;
+	b_info.nicks = NULL;
+	b_info.servers = NULL;
+	b_info.b_config = NULL;
+	b_info.config_count = 0;
+	
+	int ret = get_settings("bot.conf", &b_info);
+	
+	if((setting_ptr = get_setting("NICKS", &b_info)) == NULL)
+	{
+		printf("ERROR: No nicknames defined in the config file.\n");
+		return 0;
+	}
+	
+	/* Todo: Error handling */
+	parse_nicks(setting_ptr->value, &b_info);
+	
+	for(i = 0; i < b_info.nick_count; i++)
+	{
+		printf("%s\n", b_info.nicks[i]);
+	}
+	
 	
 	printf("ret: %d\n", ret);
 	
