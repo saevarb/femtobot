7594e8ead64d9408644c48e763be7c4b0b159c90
wouter.h.f <wouter.h.f@d742501c-4806-11de-a4f5-9d9bc3506571>
2009-05-25 07:13:16 +0000

    renamed get_settings to parse_file to avoid confusion with get_setting

--- prototypes.h
+++ prototypes.h
@@ -4,7 +4,7 @@
  * structs in their parameter list */
 
 
-int 		get_settings(char *filename, bot_info *b_info);
+int 		parse_file(char *filename, bot_info *b_info);
 bot_config *get_setting(const char *setting, bot_info *b_info);
 int 		parse_nicks(char *nicks_setting, bot_info *b_info);
 int 		parse_channels(char *channels_setting, bot_info *b_info);
