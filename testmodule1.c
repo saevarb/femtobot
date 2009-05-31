336e064b258875c7958815290e4d400ec7981e8f
icedane <icedane@d742501c-4806-11de-a4f5-9d9bc3506571>
2009-05-31 23:53:52 +0000

    Forgot to add code for testmodules.

---
+++ testmodule1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+void *PRIVMSG_test1(void *lol)
+{
+	return NULL;
+}
+
+void *PING_test1(void *lol)
+{
+	return NULL;
+}
+
+void *NOTICE_test1(void *lol)
+{
+	return NULL;
+}
---
+++ testmodule2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+void *PRIVMSG_test2(void *lol)
+{
+	return NULL;
+}
+
+void *PING_test2(void *lol)
+{
+	return NULL;
+}
+
+void *NOTICE_test2(void *lol)
+{
+	return NULL;
+}
