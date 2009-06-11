4a00d101889c2da9046267046c1e6f842711f412
icedane <icedane@d742501c-4806-11de-a4f5-9d9bc3506571>
2009-06-11 00:06:48 +0000

    Forgot some files -_-

---
+++ trunk/reader_thread.h
@@ -0,0 +1,14 @@
+#ifndef __READER_THREAD_H_
+#define __READER_THREAD_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
+#include <errno.h>
+
+#include "structs.h"
+
+
+#endif
---
+++ trunk/writer_thread.h
@@ -0,0 +1,13 @@
+#ifndef __WRITER_THREAD_H_
+#define __WRITER_THREAD_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
+#include <errno.h>
+
+#include "structs.h"
+
+#endif
